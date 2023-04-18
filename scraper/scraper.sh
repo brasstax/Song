c91d25d50781fe3e6db20e219df4695548ca860c
Brass Tax <brasstax@users.noreply.github.com>
2023-04-17 22:16:07 -0700

    Switches scraper to use zq, and adds reply_id and reply_user to database.

--- scraper/scraper.sh
+++ scraper/scraper.sh
@@ -7,8 +7,8 @@ which snscrape > /dev/null
 [[ $? -ne 0 ]] && echo "SNScrape not installed." && exit 1;
 which psql > /dev/null
 [[ $? -ne 0 ]] && echo "psql not installed." && exit 1;
-which jq > /dev/null
-[[ $? -ne 0 ]] && echo "jq not installed." && exit 1;
+which zq > /dev/null
+[[ $? -ne 0 ]] && echo "zq not installed." && exit 1;
 [[ -z ${SNSCRAPE_TWITTER_USERS} ]]  && echo "SNSCRAPE_TWITTER_USERS not set. Set SNSCRAPE_TWITTER_USERS with user handles you want to scrape, separated by comma." && exit 1;
 [[ -z ${SNSCRAPE_DATABASE_DB} ]]  && echo "SNSCRAPE_DATABASE_DB not set. Set the name for a postgres database that scraper.sh will use." && exit 1;
 [[ -z ${SNSCRAPE_DATABASE_HOST} ]] && echo "SNSCRAPE_DATABASE_HOST not set. Set the host for a postgres host that scraper.sh will use." && exit 1;
@@ -18,6 +18,8 @@ echo "Initializing SNScrape for twitter accounts ${SNSCRAPE_TWITTER_USERS}."
 echo "SELECT 'CREATE DATABASE ${SNSCRAPE_DATABASE_DB}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${SNSCRAPE_DATABASE_DB}')\gexec" | psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST"
 psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS tweets (username TEXT, status_id BIGINT, date TIMESTAMP WITH TIME ZONE, silva_read BOOLEAN, CONSTRAINT username_status_id UNIQUE (username, status_id))" > /dev/null
 psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "ALTER TABLE tweets ADD COLUMN IF NOT EXISTS hashtags text ARRAY NULL" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "ALTER TABLE tweets ADD COLUMN IF NOT EXISTS reply_id BIGINT NULL" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "ALTER TABLE tweets ADD COLUMN IF NOT EXISTS reply_user TEXT NULL" > /dev/null
 psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS muted_hashtags (id SERIAL PRIMARY KEY, hashtag TEXT)" > /dev/null
 psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS muted_users (username text UNIQUE)" > /dev/null
 psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS settings(id SERIAL PRIMARY KEY, name TEXT, value TEXT)" > /dev/null
@@ -30,12 +32,12 @@ while true; do
     echo "Scraping $user"
     IFS=' ' mapfile -t tweets < <(snscrape -n 10 --retry 3 --jsonl twitter-search "from:$user include:nativeretweets")
     for tweet in "${tweets[@]}"; do
-      username=$(echo $tweet | jq -r '.["user"]["username"]')
-      url=$(echo $tweet | jq '.["url"]')
-      IFS='/"' read -ra url_breakout <<< $url
-      status_id=${url_breakout[-1]}
-      date=$(echo $tweet | jq -r '.["date"]')
-      psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "INSERT INTO tweets(username, status_id, date, silva_read) VALUES ('$username', $status_id, '$date', false) ON CONFLICT (username, status_id) DO NOTHING" > /dev/null
+      username=$(echo $tweet | zq -f zson 'yield user.username' -)
+      status_id=$(echo $tweet | zq -f zson 'yield id' -)
+      reply_id=$(echo $tweet | zq -f zson 'yield inReplyToTweetId' -)
+      reply_user=$(echo $tweet | zq -f zson 'yield inReplyToUser.username' -)
+      date=$(echo $tweet | zq -f zson 'yield date' -)
+      psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "INSERT INTO tweets(username, status_id, date, reply_id, reply_user, silva_read) VALUES ('$username', $status_id, '$date', '$reply_id', '$reply_user', false) ON CONFLICT (username, status_id) DO NOTHING" > /dev/null
     done
   done
   sleep 2
