e9ce3cebc672784e6a821c5fc9df8d20b9778dc9
Brass Tax <brasstax@users.noreply.github.com>
2023-04-22 10:37:45 -0700

    Converts scraper to use nitter RSS feed instead of snscrape...for now.

---
+++ nitter-scraper/feed_parse.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+import os
+import signal
+import sys
+import asyncio
+import logging
+import feedparser
+from psycopg_pool import AsyncConnectionPool
+
+logging.basicConfig(level=logging.INFO)
+
+
+TO_FOLLOW = os.environ.get("SNSCRAPE_TWITTER_USERS", None)
+if not TO_FOLLOW:
+    sys.exit("SNSCRAPE_TWITTER_USERS environment variable not set.")
+
+SNSCRAPE_DATABASE_DB = os.environ.get("SNSCRAPE_DATABASE_DB", None)
+if not SNSCRAPE_DATABASE_DB:
+    sys.exit("SNSCRAPE_DATABASE_DB environment variable not set.")
+
+SNSCRAPE_DATABASE_HOST = os.environ.get("SNSCRAPE_DATABASE_HOST", None)
+if not SNSCRAPE_DATABASE_HOST:
+    sys.exit("SNSCRAPE_DATABASE_HOST environment variable not set.")
+
+SNSCRAPE_DATABASE_USERNAME = os.environ.get("SNSCRAPE_DATABASE_USERNAME", None)
+if not SNSCRAPE_DATABASE_USERNAME:
+    sys.exit("SNSCRAPE_DATABASE_USERNAME environment variable not set.")
+
+SNSCRAPE_DATABASE_PASSWORD = os.environ.get("SNSCRAPE_DATABASE_PASSWORD", None)
+if not SNSCRAPE_DATABASE_PASSWORD:
+    sys.exit("SNSCRAPE_DATABASE_PASSWORD environment variable not set.")
+
+NITTER_ADDRESS = os.environ.get("NITTER_ADDRESS", None)
+if not NITTER_ADDRESS:
+    sys.exit("NITTER_ADDRESS environment variable not set.")
+
+conn_str = f"user={SNSCRAPE_DATABASE_USERNAME} password={SNSCRAPE_DATABASE_PASSWORD} dbname={SNSCRAPE_DATABASE_DB} host={SNSCRAPE_DATABASE_HOST}"
+
+async def amain():
+  # Block below commented out where I eventually get reply IDs and users working with nitter;
+  # for now I'd like to get it to just work again.
+  # cmd = """
+  # INSERT INTO tweets(username, status_id, date, reply_id, reply_user, silva_read)
+  # VALUES ('$username', $status_id, '$date', NULLIF('$reply_id', '0')::bigint, NULLIF('$reply_user', ''), false)
+  # ON CONFLICT (username, status_id) DO NOTHING
+  # """
+  conn = AsyncConnectionPool(conn_str)
+  cmd = """
+  INSERT INTO tweets(username, status_id, date, silva_read)
+  VALUES (%s, %s, %s, false)
+  ON CONFLICT (username, status_id) DO NOTHING
+  """
+  for username in TO_FOLLOW.split(","):
+    feed = feedparser.parse(f"http://{NITTER_ADDRESS}/{username}/with_replies/rss")
+    async with conn.connection() as db:
+      for entry in feed["entries"]:
+        logging.debug(entry)
+        tweet_id = int(entry.id.split("/")[-1].split("#")[0])
+        published = entry["published"]
+        async with db.cursor() as cur:
+            logging.info(f"Parse {username} for ID {tweet_id} published {published}, titled {entry['title']}")
+            await cur.execute(cmd, (username, tweet_id, published,), prepare=True)
+
+def ask_exit():
+    loop = asyncio.get_event_loop()
+    for task in asyncio.Task.all_tasks():
+        task.cancel()
+    loop.stop()
+
+if __name__ == "__main__":
+    asyncio.run(amain())
---
+++ nitter-scraper/scraper.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+which psql > /dev/null
+[[ $? -ne 0 ]] && echo "psql not installed." && exit 1;
+which zq > /dev/null
+[[ $? -ne 0 ]] && echo "zq not installed." && exit 1;
+[[ -z ${SNSCRAPE_TWITTER_USERS} ]]  && echo "SNSCRAPE_TWITTER_USERS not set. Set SNSCRAPE_TWITTER_USERS with user handles you want to scrape, separated by comma." && exit 1;
+[[ -z ${SNSCRAPE_DATABASE_DB} ]]  && echo "SNSCRAPE_DATABASE_DB not set. Set the name for a postgres database that scraper.sh will use." && exit 1;
+[[ -z ${SNSCRAPE_DATABASE_HOST} ]] && echo "SNSCRAPE_DATABASE_HOST not set. Set the host for a postgres host that scraper.sh will use." && exit 1;
+[[ -z ${SNSCRAPE_DATABASE_USERNAME} ]] && echo "SNSCRAPE_DATABASE_USERNAME not set. Set the username for a postgres host that scraper.sh will use." && exit 1;
+[[ -z ${SNSCRAPE_DATABASE_PASSWORD} ]] && echo "SNSCRAPE_DATABASE_PASSWORD not set. Set the password for a postgres host that scraper.sh will use." && exit 1;
+echo "Initializing SNScrape for twitter accounts ${SNSCRAPE_TWITTER_USERS}."
+echo "SELECT 'CREATE DATABASE ${SNSCRAPE_DATABASE_DB}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${SNSCRAPE_DATABASE_DB}')\gexec" | psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST"
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS tweets (username TEXT, status_id BIGINT, date TIMESTAMP WITH TIME ZONE, silva_read BOOLEAN, CONSTRAINT username_status_id UNIQUE (username, status_id))" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "ALTER TABLE tweets ADD COLUMN IF NOT EXISTS hashtags text ARRAY NULL" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "ALTER TABLE tweets ADD COLUMN IF NOT EXISTS reply_id BIGINT NULL" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "ALTER TABLE tweets ADD COLUMN IF NOT EXISTS reply_user TEXT NULL" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS muted_hashtags (id SERIAL PRIMARY KEY, hashtag TEXT)" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS muted_users (username text UNIQUE)" > /dev/null
+psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS settings(id SERIAL PRIMARY KEY, name TEXT, value TEXT)" > /dev/null
+while true; do
+  ./feed_parse.py
+  sleep 20
+done
