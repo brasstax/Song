8445eb25f0fdd2bc8f1e2d4167c8df10002da6cc
Brass Tax <brasstax@users.noreply.github.com>
2023-04-22 10:37:04 -0700

    Removes extraneous redirect from healthcheck.

--- scraper/healthcheck.sh
+++ scraper/healthcheck.sh
@@ -4,4 +4,4 @@
 which psql > /dev/null
 [[ $? -ne 0 ]] && echo "psql not installed." && exit 1;
 
-PGPASSWORD=$SNSCRAPE_DATABASE_PASSWORD psql -U $SNSCRAPE_DATABASE_USERNAME -h $SNSCRAPE_DATABASE_HOST $SNSCRAPE_DATABASE_DB -c "select version();" > /dev/null > 2&>1
+PGPASSWORD=$SNSCRAPE_DATABASE_PASSWORD psql -U $SNSCRAPE_DATABASE_USERNAME -h $SNSCRAPE_DATABASE_HOST $SNSCRAPE_DATABASE_DB -c "select version();" > /dev/null
