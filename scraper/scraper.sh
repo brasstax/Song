93f2a10972a47f30915b131fbaddcb0a9706c781
Brass Tax <brasstax@users.noreply.github.com>
2023-06-19 12:27:04 -0700

    adds a SNSCRAPE_MAX_RESULTS variable in case you don't want to scrape really really far back. Defaults to 10.

--- scraper/scraper.sh
+++ scraper/scraper.sh
@@ -14,6 +14,7 @@ which zq > /dev/null
 [[ -z ${SNSCRAPE_DATABASE_HOST} ]] && echo "SNSCRAPE_DATABASE_HOST not set. Set the host for a postgres host that scraper.sh will use." && exit 1;
 [[ -z ${SNSCRAPE_DATABASE_USERNAME} ]] && echo "SNSCRAPE_DATABASE_USERNAME not set. Set the username for a postgres host that scraper.sh will use." && exit 1;
 [[ -z ${SNSCRAPE_DATABASE_PASSWORD} ]] && echo "SNSCRAPE_DATABASE_PASSWORD not set. Set the password for a postgres host that scraper.sh will use." && exit 1;
+[[ -z ${SNSCRAPE_MAX_RESULTS} ]] && set SNSCRAPE_MAX_RESULTS=10;
 echo "Initializing SNScrape for twitter accounts ${SNSCRAPE_TWITTER_USERS}."
 echo "SELECT 'CREATE DATABASE ${SNSCRAPE_DATABASE_DB}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${SNSCRAPE_DATABASE_DB}')\gexec" | psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST"
 psql "user=$SNSCRAPE_DATABASE_USERNAME password=$SNSCRAPE_DATABASE_PASSWORD host=$SNSCRAPE_DATABASE_HOST dbname=$SNSCRAPE_DATABASE_DB" -c "CREATE TABLE IF NOT EXISTS tweets (username TEXT, status_id BIGINT, date TIMESTAMP WITH TIME ZONE, silva_read BOOLEAN, CONSTRAINT username_status_id UNIQUE (username, status_id))" > /dev/null
@@ -30,8 +31,8 @@ trap "exit" SIGINT
 while true; do
   for user in "${USERS[@]}"; do
     echo "Scraping $user"
-    IFS=' ' mapfile -t tweets < <(snscrape -n 10 --retry 3 --jsonl twitter-search "from:$user include:nativeretweets")
-    IFS=' ' mapfile -t -O "${#tweets[@]}" tweets < <(snscrape -n 10 --retry 3 --jsonl twitter-profile $user)
+    IFS=' ' mapfile -t tweets < <(snscrape --max-results $SNSCRAPE_MAX_RESULTS --retry 3 --jsonl twitter-search "from:$user include:nativeretweets")
+    IFS=' ' mapfile -t -O "${#tweets[@]}" tweets < <(snscrape --max-results $SNSCRAPE_MAX_RESULTS --retry 3 --jsonl twitter-profile $user)
     for tweet in "${tweets[@]}"; do
       username=$(echo $tweet | zq -f zson 'yield user.username' - | sed s/\"//g)
       status_id=$(echo $tweet | zq -f zson 'yield id' -)
